c42ae8f715477ea71eb28b1a27ca786a98262395
Vijo Cherian <codervijo@gmail.com>
2019-11-12 18:10:49 -0800

    build output

---
+++ lahistory/output/precache-manifest.728c1d98823a5f2a69385268ba46f918.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1296bddca739a2438e2d3e255e425e8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c09df158d87b00c8aee1",
+    "url": "/static/css/main.47e6ca29.chunk.css"
+  },
+  {
+    "revision": "779fdbfd3042d32ff665",
+    "url": "/static/js/2.8366aa0b.chunk.js"
+  },
+  {
+    "revision": "c09df158d87b00c8aee1",
+    "url": "/static/js/main.f45e315b.chunk.js"
+  },
+  {
+    "revision": "10a313eda97c539b42a9",
+    "url": "/static/js/runtime-main.1641e7e5.js"
+  }
+]);
